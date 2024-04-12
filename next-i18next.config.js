c3daca0585be9186fdd4e677303fafc9be926690
emmajane1313 <admin@digitalax.xyz>
2024-04-12 17:46:56 -0400

    otro locale

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next-i18next').UserConfig} */
+const path = require("path");
 
 module.exports = {
   i18n: {
     defaultLocale: "en",
     locales: ["en", "es", "ar"],
+    localePath: path.resolve("./public/locales"),
   },
   react: {
     useSuspense: false,
