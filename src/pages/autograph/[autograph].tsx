b1cc8b25595865fe4d9b7709b1101aa14f086544
emmajane1313 <admin@digitalax.xyz>
2024-04-12 19:56:20 -0400

    minor

--- src/pages/autograph/[autograph].tsx
+++ src/pages/autograph/[autograph].tsx
@@ -717,7 +717,7 @@ const Autograph: NextPage<{
                 questsLoading={questsLoading}
                 questSample={questSample}
               />
-              <div className="relative flex flex-row gap-12 otro:gap-3 items-start justify-between sm:px-4 w-full h-full otro:flex-nowrap flex-wrap">
+              {/* <div className="relative flex flex-row gap-12 otro:gap-3 items-start justify-between sm:px-4 w-full h-full otro:flex-nowrap flex-wrap">
                 <Feed
                   t={t}
                   tCom={tCom}
@@ -801,7 +801,7 @@ const Autograph: NextPage<{
                     objectFit="contain"
                   />
                 </div>
-              </div>
+              </div> */}
             </div>
           )
         )}
