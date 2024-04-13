5c3c0153e96f69fee079f79bb57e554819c17a1c
emmajane1313 <admin@digitalax.xyz>
2024-04-13 01:29:10 -0400

    prueba

--- src/pages/autograph/[autograph].tsx
+++ src/pages/autograph/[autograph].tsx
@@ -30,111 +30,111 @@ const Autograph: NextPage<{
   tCom: TFunction<"common", undefined>;
   i18n: i18n;
 }> = ({ router, client, tCom, i18n }): JSX.Element => {
-  const dispatch = useDispatch();
-  const { address, isConnected } = useAccount();
-  const params = useParams<{ autograph: string }>();
-  const publicClient = createPublicClient({
-    chain: polygon,
-    transport: http(
-      `https://polygon-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`
-    ),
-  });
-  const [globalLoading, setGlobalLoading] = useState<boolean>(true);
-  const fullScreenVideo = useSelector(
-    (state: RootState) => state.app.fullScreenVideoReducer
-  );
-  const filterConstants = useSelector(
-    (state: RootState) => state.app.filterConstantsReducer.items
-  );
-  const filters = useSelector(
-    (state: RootState) => state.app.filterReducer.filter
-  );
-  const allSearchItems = useSelector(
-    (state: RootState) => state.app.searchItemsReducer
-  );
-  const cartItems = useSelector(
-    (state: RootState) => state.app.cartItemsReducer.items
-  );
-  const cartAnim = useSelector(
-    (state: RootState) => state.app.cartAnimReducer.value
-  );
-  const searchActive = useSelector(
-    (state: RootState) => state.app.searchActiveReducer.value
-  );
-  const filtersOpen = useSelector(
-    (state: RootState) => state.app.filtersOpenReducer
-  );
-  const lensConnected = useSelector(
-    (state: RootState) => state.app.lensConnectedReducer.profile
-  );
-  const oracleData = useSelector(
-    (state: RootState) => state.app.oracleDataReducer.data
-  );
-  const walletConnected = useSelector(
-    (state: RootState) => state.app.walletConnectedReducer.value
-  );
-  const { profileLoading, profile } = useAutograph(
-    params?.autograph,
-    lensConnected
-  );
+  // const dispatch = useDispatch();
+  // const { address, isConnected } = useAccount();
+  // const params = useParams<{ autograph: string }>();
+  // const publicClient = createPublicClient({
+  //   chain: polygon,
+  //   transport: http(
+  //     `https://polygon-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`
+  //   ),
+  // });
+  // const [globalLoading, setGlobalLoading] = useState<boolean>(true);
+  // const fullScreenVideo = useSelector(
+  //   (state: RootState) => state.app.fullScreenVideoReducer
+  // );
+  // const filterConstants = useSelector(
+  //   (state: RootState) => state.app.filterConstantsReducer.items
+  // );
+  // const filters = useSelector(
+  //   (state: RootState) => state.app.filterReducer.filter
+  // );
+  // const allSearchItems = useSelector(
+  //   (state: RootState) => state.app.searchItemsReducer
+  // );
+  // const cartItems = useSelector(
+  //   (state: RootState) => state.app.cartItemsReducer.items
+  // );
+  // const cartAnim = useSelector(
+  //   (state: RootState) => state.app.cartAnimReducer.value
+  // );
+  // const searchActive = useSelector(
+  //   (state: RootState) => state.app.searchActiveReducer.value
+  // );
+  // const filtersOpen = useSelector(
+  //   (state: RootState) => state.app.filtersOpenReducer
+  // );
+  // const lensConnected = useSelector(
+  //   (state: RootState) => state.app.lensConnectedReducer.profile
+  // );
+  // const oracleData = useSelector(
+  //   (state: RootState) => state.app.oracleDataReducer.data
+  // );
+  // const walletConnected = useSelector(
+  //   (state: RootState) => state.app.walletConnectedReducer.value
+  // );
+  // const { profileLoading, profile } = useAutograph(
+  //   params?.autograph,
+  //   lensConnected
+  // );
 
-  const { openConnectModal } = useConnectModal();
-  const { openAccountModal } = useAccountModal();
-  const {
-    handleLensConnect,
-    openAccount,
-    setOpenAccount,
-    signInLoading,
-    cartListOpen,
-    setCartListOpen,
-    handleLogout,
-  } = useSignIn(
-    publicClient,
-    address,
-    isConnected,
-    dispatch,
-    oracleData,
-    cartItems,
-    lensConnected,
-    cartAnim,
-    openAccountModal
-  );
-  const { handleShuffleSearch } = useSearch(
-    filtersOpen,
-    lensConnected,
-    searchActive,
-    filterConstants,
-    filters,
-    allSearchItems,
-    dispatch,
-    router
-  );
+  // const { openConnectModal } = useConnectModal();
+  // const { openAccountModal } = useAccountModal();
+  // const {
+  //   handleLensConnect,
+  //   openAccount,
+  //   setOpenAccount,
+  //   signInLoading,
+  //   cartListOpen,
+  //   setCartListOpen,
+  //   handleLogout,
+  // } = useSignIn(
+  //   publicClient,
+  //   address,
+  //   isConnected,
+  //   dispatch,
+  //   oracleData,
+  //   cartItems,
+  //   lensConnected,
+  //   cartAnim,
+  //   openAccountModal
+  // );
+  // const { handleShuffleSearch } = useSearch(
+  //   filtersOpen,
+  //   lensConnected,
+  //   searchActive,
+  //   filterConstants,
+  //   filters,
+  //   allSearchItems,
+  //   dispatch,
+  //   router
+  // );
 
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      if (
-        !profileLoading
-        //  && !feedLoading && !galleryLoading
-      ) {
-        setGlobalLoading(false);
-      }
-    }, 1000);
+  // useEffect(() => {
+  //   const timeoutId = setTimeout(() => {
+  //     if (
+  //       !profileLoading
+  //       //  && !feedLoading && !galleryLoading
+  //     ) {
+  //       setGlobalLoading(false);
+  //     }
+  //   }, 1000);
 
-    return () => clearTimeout(timeoutId);
-  }, [
-    profileLoading,
-    // , feedLoading, galleryLoading
-  ]);
+  //   return () => clearTimeout(timeoutId);
+  // }, [
+  //   profileLoading,
+  //   // , feedLoading, galleryLoading
+  // ]);
 
-  if (
-    !profileLoading &&
-    !globalLoading &&
-    profile &&
-    // !feedLoading &&
-    // !galleryLoading &&
-    i18n.isInitialized
-  ) {
-    return (
+  // if (
+  //   !profileLoading &&
+  //   !globalLoading &&
+  //   profile &&
+  //   // !feedLoading &&
+  //   // !galleryLoading &&
+  //   i18n.isInitialized
+  // ) {
+    return ( <></>
       // <>
       //   {!profile ? (
       //     <NotFound
@@ -161,31 +161,31 @@ const Autograph: NextPage<{
       //     />
       //   ) : (
       // profile && (
-      <AutoHydrateSSR
-        router={router}
-        tCom={tCom}
-        dispatch={dispatch}
-        lensConnected={lensConnected}
-        publicClient={publicClient}
-        address={address}
-        client={client}
-        profile={profile!}
-        cartItems={cartItems}
-        openConnectModal={openConnectModal}
-        handleLensConnect={handleLensConnect}
-        walletConnected={walletConnected}
-        handleLogout={handleLogout}
-        filterConstants={filterConstants}
-        handleShuffleSearch={handleShuffleSearch}
-      />
+      // <AutoHydrateSSR
+      //   router={router}
+      //   tCom={tCom}
+      //   dispatch={dispatch}
+      //   lensConnected={lensConnected}
+      //   publicClient={publicClient}
+      //   address={address}
+      //   client={client}
+      //   profile={profile!}
+      //   cartItems={cartItems}
+      //   openConnectModal={openConnectModal}
+      //   handleLensConnect={handleLensConnect}
+      //   walletConnected={walletConnected}
+      //   handleLogout={handleLogout}
+      //   filterConstants={filterConstants}
+      //   handleShuffleSearch={handleShuffleSearch}
+      // />
     );
     // )
     // }
     //   </>
     // );
-  }
+  // }
 
-  return <RouterChange />;
+  // return <RouterChange />;
 };
 
 export default Autograph;
