2db58608b3a5588787ce6db0940a0d35c9b230c7
emmajane1313 <emma.jane@digitalax.xyz>
2025-06-11 17:26:14 -0400

    nf

--- src/app/[lang]/[...notFound]/page.tsx
+++ src/app/[lang]/[...notFound]/page.tsx
@@ -4,7 +4,7 @@ import NotFoundEntry from "@/app/components/Common/modules/NotFoundEntry";
 import RouterChange from "@/app/components/Common/modules/RouterChange";
 import { getDictionary } from "../dictionaries";
 
-export default async function IndexPage({ params }: { params: tParams }) {
+export default async function NotFound({ params }: { params: tParams }) {
   const { lang } = await params;
   const dict = await (getDictionary as (locale: any) => Promise<any>)(lang);
   return (
